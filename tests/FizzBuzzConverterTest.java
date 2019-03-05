ef07341ca73b1f8bc299325247cb2d80a86e330a
Erin Stevenson <estevenson_94@live.com>
2019-03-05 21:54:02 +0000

    Tests refactor to include before method

--- tests/FizzBuzzConverterTest.java
+++ tests/FizzBuzzConverterTest.java
@@ -1,31 +1,34 @@
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class FizzBuzzConverterTest {
 
   private FizzBuzzConverter fizzBuzz;
 
+  @Before
+  public void beforeEachTestMethod() {
+      this.fizzBuzz = new FizzBuzzConverter();
+  }
+
   @Test
   public void converterReturnsOneWhenGivenOne() {
-      this.fizzBuzz = new FizzBuzzConverter();
       assertEquals("1", fizzBuzz.convert(1));
   }
 
    @Test
    public void converterReturnsFizzWhenGivenThree() {
-       this.fizzBuzz = new FizzBuzzConverter();
        assertEquals("Fizz", fizzBuzz.convert(3));
    }
 
     @Test
     public void converterReturnsBuzzWhenGivenFive() {
-        this.fizzBuzz = new FizzBuzzConverter();
         assertEquals("Buzz", fizzBuzz.convert(5));
     }
 
     @Test
     public void converterReturnsFizzBuzzWhenGivenFifteen() {
-        this.fizzBuzz = new FizzBuzzConverter();
         assertEquals("FizzBuzz", fizzBuzz.convert(15));
     }
 }
