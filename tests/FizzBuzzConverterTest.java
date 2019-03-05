36507c73e0fcb0b2429819f106ed75d0a13bc357
Erin Stevenson <estevenson_94@live.com>
2019-03-05 21:58:21 +0000

    All multiples of three upto 100 give fizz

--- tests/FizzBuzzConverterTest.java
+++ tests/FizzBuzzConverterTest.java
@@ -22,13 +22,22 @@ public class FizzBuzzConverterTest {
        assertEquals("Fizz", fizzBuzz.convert(3));
    }
 
-    @Test
-    public void converterReturnsBuzzWhenGivenFive() {
-        assertEquals("Buzz", fizzBuzz.convert(5));
-    }
+   @Test
+   public void converterReturnsBuzzWhenGivenFive() {
+      assertEquals("Buzz", fizzBuzz.convert(5));
+   }
 
-    @Test
-    public void converterReturnsFizzBuzzWhenGivenFifteen() {
-        assertEquals("FizzBuzz", fizzBuzz.convert(15));
-    }
+   @Test
+   public void converterReturnsFizzBuzzWhenGivenFifteen() {
+      assertEquals("FizzBuzz", fizzBuzz.convert(15));
+   }
+
+   @Test
+    public void allMutliplesOfThreeButNotFiveGiveFizz() {
+      for (int i = 1; i <= 100; i++) {
+          if ((i % 3 == 0) && !(i % 5 == 0)) {
+              assertEquals("Fizz", fizzBuzz.convert(i));
+          }
+      }
+   }
 }
