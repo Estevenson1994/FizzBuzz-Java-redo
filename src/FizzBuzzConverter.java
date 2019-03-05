42f3f683b953c5b6eb84304f75ad8051657c6bde
Erin Stevenson <estevenson_94@live.com>
2019-03-05 22:03:57 +0000

    refactor FizzBuzzConverter

--- src/FizzBuzzConverter.java
+++ src/FizzBuzzConverter.java
@@ -1,16 +1,9 @@
 public class FizzBuzzConverter {
     public String convert(int number) {
-        if (number%3==0 && number%5==0) {
-            return "FizzBuzz";
-        }
-        else if (number%3==0) {
-            return "Fizz";
-        }
-        else if (number%5==0) {
-            return "Buzz";
-        }
-        else {
-            return String.valueOf(number);
-        }
+        String result = "";
+        result += ((number % 3 == 0) ? "Fizz" : "");
+        result += ((number % 5 == 0) ? "Buzz" : "");
+        result = ((result.equals("")) ? String.valueOf(number) : result);
+        return result;
     }
 }
